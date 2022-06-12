d82b115d079caa6171614015e144462fa9c0a1b1
Kerem Güneş <qeremy@gmail.com>
2022-06-12 23:41:42 +0300

    Add file.object.TempFileObject tests.

---
+++ file/object/TempFileObjectTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+namespace froq\test\file;
+use froq\file\object\{TempFileObject, TempFileObjectException};
+
+class TempFileObjectTest extends \TestCase
+{
+    function test_constructor() {
+        $fo = new TempFileObject();
+
+        $this->assertIsResource($fo->getResource());
+        $this->assertFileExists($fo->getResourceFile());
+
+        $fo->free(); // Clean up.
+
+        $this->assertIsNotResource($fo->getResource());
+        $this->assertFileNotExists((string) $fo->getResourceFile());
+    }
+
+    function test_openClose() {
+        $fo = new TempFileObject();
+
+        try {
+            $fo->open();
+        } catch (TempFileObjectException $e) {
+            $this->assertSame(sprintf(
+                'Method open() is not available for %s classes', TempFileObject::class
+            ), $e->getMessage());
+        }
+
+        try {
+            $fo->close();
+        } catch (TempFileObjectException $e) {
+            $this->assertSame(sprintf(
+                'Method close() is not available for %s classes', TempFileObject::class
+            ), $e->getMessage());
+        }
+    }
+}
