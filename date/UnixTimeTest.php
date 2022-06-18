59ece55e2b1e68a010c70e7e95fbec4c943f11a0
Kerem Güneş <qeremy@gmail.com>
2022-06-18 16:20:42 +0300

    Update date.UnixTime tests.

--- date/UnixTimeTest.php
+++ date/UnixTimeTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 namespace test\froq\date;
-use froq\date\UnixTime;
+use froq\date\{UnixTime, Date};
+use DateTime;
 
 class UnixTimeTest extends \TestCase
 {
@@ -10,7 +11,12 @@ class UnixTimeTest extends \TestCase
 
     function test_from() {
         $when = '1990-01-09 23:30:11.506001 +00:00';
+        $time = strtotime($when);
+
+        $this->assertSame($time, UnixTime::from($when));
         $this->assertSame(631927811, UnixTime::from($when));
-        $this->assertSame(strtotime($when), UnixTime::from($when));
+
+        $this->assertSame($time, UnixTime::from(new Date($when)));
+        $this->assertSame($time, UnixTime::from(new DateTime($when)));
     }
 }
