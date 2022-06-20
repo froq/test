cad198733ec05bd61171a847b957e3872ea3989a
Kerem Güneş <qeremy@gmail.com>
2022-06-20 17:58:00 +0300

    Add more / update date.Dater tests.

--- date/DaterTest.php
+++ date/DaterTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 namespace test\froq\date;
-use froq\date\{Dater, Date, UtcDate};
+use froq\date\{Dater, Date, UtcDate, DateException};
 
 class DaterTest extends \TestCase
 {
@@ -8,17 +8,20 @@ class DaterTest extends \TestCase
         $when = '1990-01-09 23:30:11 +00:00';
 
         ['year' => $year, 'month' => $month, 'day' => $day,
-         'hour' => $hour, 'minute' => $minute , 'second' => $second] = Date::parse($when);
+         'hour' => $hour, 'minute' => $minute , 'second' => $second
+        ] = Date::parse($when);
 
         $dater = new Dater(
             year: $year, month: $month, day: $day,
             hour: $hour, minute: $minute, second: $second,
         );
+
         $this->assertSame($when, $dater->format('Y-m-d H:i:s P'));
     }
 
     function test_settersGetters() {
         $dater = new Dater();
+
         $this->assertNull($dater->getYear());
         $this->assertNull($dater->getMonth());
         $this->assertNull($dater->getDay());
@@ -53,31 +56,60 @@ class DaterTest extends \TestCase
 
     function test_converters() {
         $dater = new Dater();
+
         $this->assertInstanceOf(Date::class, $dater->toDate());
         $this->assertInstanceOf(UtcDate::class, $dater->toUtcDate());
     }
 
     function test_format() {
         $dater = new Dater();
+
         $format = 'Y-m-d H:i:s';
         $this->assertSame(date($format), $dater->format($format));
         $this->assertSame(gmdate($format), $dater->formatUtc($format));
     }
 
+    function test_setDate() {
+        $dater = new Dater();
+        $dater->setDate('1990-01-09');
+
+        $this->assertSame('1990-01-09', $dater->getDate());
+        $this->assertSame('1990-01-09 00:00:00.000000', $dater->getFullDate());
+
+        $this->expectException(DateException::class);
+        $this->expectExceptionMessage('Invalid date: foo');
+        $dater->setDate('foo');
+    }
+
     function test_getDate() {
         $dater = new Dater(1990, 1, 9, 23, 30, 11, 506001);
+
         $this->assertSame('1990-01-09', $dater->getDate());
         $this->assertSame('1990-01-09 23:30:11.506001', $dater->getFullDate());
     }
 
+    function test_setTime() {
+        $dater = new Dater();
+        $dater->setTime('23:30:11');
+
+        $this->assertSame('23:30', $dater->getTime());
+        $this->assertSame('23:30:11.000000', $dater->getFullTime());
+
+        $this->expectException(DateException::class);
+        $this->expectExceptionMessage('Invalid time: foo');
+        $dater->setTime('foo');
+    }
+
     function test_getTime() {
         $dater = new Dater(1990, 1, 9, 23, 30, 11, 506001);
+
         $this->assertSame('23:30', $dater->getTime());
         $this->assertSame('23:30:11.506001', $dater->getFullTime());
     }
 
     function test_getTimestamp() {
         $dater = new Dater(1990, 1, 9, 23, 30, 11, 506001);
+
         $this->assertSame(631927811, $dater->getTimestamp());
         $this->assertSame(631927811.506001, $dater->getTimestamp(true));
     }
