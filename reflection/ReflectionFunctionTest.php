5e304e22c0a4d97d43e197b9bb06630e3177edaa
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-08-01 23:45:12 +0300

    Add reflection.ReflectionFunction tests.

---
+++ reflection/ReflectionFunctionTest.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+namespace test\froq\reflection;
+use froq\reflection\ReflectionFunction;
+
+class ReflectionFunctionTest extends \TestCase
+{
+    function test_getters() {
+        $ref = new ReflectionFunction('strlen');
+        $this->assertSame('strlen', $ref->getName());
+        $this->assertSame('int', $ref->getReturnType()->getName());
+
+        try {
+            $this->assertSame(__class__, $ref->getClass());
+        } catch (\ReflectionException $e) {
+            $this->assertSame('Invalid call as froq\reflection\ReflectionFunction::getClass()', $e->getMessage());
+        }
+        try {
+            $this->assertSame(__class__, $ref->getDeclaringClass()->name);
+        } catch (\ReflectionException $e) {
+            $this->assertSame('Invalid call as froq\reflection\ReflectionFunction::getDeclaringClass()', $e->getMessage());
+        }
+    }
+
+    function test_attributeMethods() {
+        $ref = new ReflectionFunction('strlen');
+        $this->assertInstanceOf(\Set::class, $ref->attributes());
+        $this->assertCount(0, $ref->attributes());
+        $this->assertFalse($ref->hasAttribute('Foo'));
+        $this->assertNull($ref->getAttribute('Foo'));
+        $this->assertSame([], $ref->getAttributeNames());
+    }
+
+    function test_parameterMethods() {
+        $ref = new ReflectionFunction('strlen');
+        $this->assertInstanceOf(\Set::class, $ref->parameters());
+        $this->assertCount(1, $ref->parameters());
+        $this->assertTrue($ref->hasParameter(0));
+        $this->assertTrue($ref->hasParameter('string'));
+        $this->assertFalse($ref->hasParameter(3));
+        $this->assertFalse($ref->hasParameter('foo'));
+        $this->assertNotNull($ref->getParameter(0));
+        $this->assertNull($ref->getParameter(3));
+        $this->assertNotNull($ref->getParameter('string'));
+        $this->assertNull($ref->getParameter('foo'));
+        $this->assertCount(1, $ref->getParameters());
+        $this->assertSame(1, $ref->getParametersCount());
+        $this->assertSame(['string'], $ref->getParameterNames());
+        $this->assertSame([null], $ref->getParameterValues());
+    }
+}
