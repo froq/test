17e964fc57f57fdef498590756a66b805fa7d631
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-11 03:34:44 +0300

    Update reflection tests.

--- reflection/ReflectionInterfaceTest.php
+++ reflection/ReflectionInterfaceTest.php
@@ -5,14 +5,14 @@ use froq\reflection\{ReflectionClass, ReflectionInterface};
 class ReflectionInterfaceTest extends \TestCase
 {
     function test_constructor() {
-        $ref = new ReflectionInterface(\Throwable::class);
+        $ref = new ReflectionInterface('Throwable');
         $this->assertFalse($ref->isClass());
         $this->assertSame('interface', $ref->getType());
         $this->assertInstanceOf(ReflectionClass::class, $ref);
         $this->assertInstanceOf(\ReflectionClass::class, $ref);
 
         $this->expectException(\ReflectionException::class);
-        $this->expectExceptionMessage('Interface "TestCase" does not exist');
-        new ReflectionInterface(\TestCase::class);
+        $this->expectExceptionMessage('Interface "Foo_Bar_Baz" does not exist');
+        new ReflectionInterface('Foo_Bar_Baz');
     }
 }
--- reflection/ReflectionTraitTest.php
+++ reflection/ReflectionTraitTest.php
@@ -5,14 +5,14 @@ use froq\reflection\{ReflectionClass, ReflectionTrait};
 class ReflectionTraitTest extends \TestCase
 {
     function test_constructor() {
-        $ref = new ReflectionTrait(\ErrorTrait::class);
+        $ref = new ReflectionTrait('froq\common\trait\ThrowableTrait');
         $this->assertFalse($ref->isClass());
         $this->assertSame('trait', $ref->getType());
         $this->assertInstanceOf(ReflectionClass::class, $ref);
         $this->assertInstanceOf(\ReflectionClass::class, $ref);
 
         $this->expectException(\ReflectionException::class);
-        $this->expectExceptionMessage('Trait "TestCase" does not exist');
-        new ReflectionTrait(\TestCase::class);
+        $this->expectExceptionMessage('Trait "Foo_Bar_Baz" does not exist');
+        new ReflectionTrait('Foo_Bar_Baz');
     }
 }
