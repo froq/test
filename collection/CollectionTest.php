7b2e732b8042f4ae2662b027619ce2c9aab19e9f
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2022-12-06 23:42:09 +0300

    Drop collection readOnly tests.

--- collection/CollectionTest.php
+++ collection/CollectionTest.php
@@ -1,7 +1,6 @@
 <?php declare(strict_types=1);
 namespace test\froq\collection;
 use froq\collection\{Collection, CollectionException};
-use froq\common\exception\ReadOnlyException;
 
 class CollectionTest extends \TestCase
 {
