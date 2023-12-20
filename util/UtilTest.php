9454d62981bc641566478acc1c71606021e76550
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-12-20 21:18:41 +0300

    Add util.Util tests.

---
+++ util/UtilTest.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+namespace test\froq\util;
+use froq\util\{Util, UtilException};
+
+class UtilTest extends \TestCase
+{
+    function testLoadSugar() {
+        $this->assertFunctionNotExists('html_encode');
+
+        Util::loadSugar('html');
+
+        $this->assertFunctionExists('html_encode');
+
+        $this->expectException(UtilException::class);
+        $this->expectExceptionMessageMatches("~^Invalid sugar name 'foo'~");
+
+        Util::loadSugar('foo');
+    }
+
+    function testGetClientIp() {
+        $_SERVER['REMOTE_ADDR'] = $ip = '1.1.1.1'; // Fake it.
+
+        $this->assertSame($ip, Util::getClientIp());
+    }
+
+    function testGetClientAgent() {
+        $_SERVER['HTTP_USER_AGENT'] = $ua = 'Test UA'; // Fake it.
+
+        $this->assertSame($ua, Util::getClientAgent());
+    }
+
+    function testGetCurrentUrl() {
+        $_SERVER['REQUEST_SCHEME'] = $scheme = 'http';
+        $_SERVER['REQUEST_URI']    = $uri    = '/a/1';
+        $_SERVER['SERVER_NAME']    = $host   = 'local';
+        $_SERVER['SERVER_PORT']    = $port   = 8080;
+
+        $url = sprintf('%s://%s:%s%s', $scheme, $host, $port, $uri);
+
+        $this->assertSame($url, Util::getCurrentUrl());
+    }
+
+    function testFormatBytes() {
+        $bytes = 1024 ** 2; // 1MB/1024KB
+
+        $this->assertSame('1024KB', Util::formatBytes($bytes));
+    }
+
+    function testConvertBytes() {
+        $bytes = '1MB'; // 1MB/1024KB
+
+        $this->assertSame(1048576, Util::convertBytes($bytes));
+    }
+
+    function testMakeClosure() {
+        $closure = Util::makeClosure(fn() => true, bind: null);
+
+        $this->assertInstanceOf(\Closure::class, $closure);
+    }
+
+    function testMakeArray() {
+        $array = ['a' => 1, 'b' => ['c' => []]];
+        $object = (object) $array;
+        $arrayObject = new \ArrayObject($array);
+
+        $this->assertEquals($array, Util::makeArray($array));
+        $this->assertEquals($array, Util::makeArray($object));
+        $this->assertEquals($array, Util::makeArray($arrayObject));
+    }
+
+    function testMakeObject() {
+        $object = (object) ['a' => 1, 'b' => ['c' => []]];
+        $array = (array) $object;
+        $arrayObject = new \ArrayObject($array);
+
+        $this->assertEquals($object, Util::makeObject($object));
+        $this->assertEquals($object, Util::makeObject($array));
+        $this->assertEquals($object, Util::makeObject($arrayObject));
+    }
+}
