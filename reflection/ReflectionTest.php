6a142288c33c74fc883548f76d79cebb367cac0b
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2024-01-07 01:49:15 +0300

    Update reflection.Reflection tests [add testGetType()].

--- reflection/ReflectionTest.php
+++ reflection/ReflectionTest.php
@@ -8,6 +8,23 @@ use froq\reflection\{
 
 class ReflectionTest extends \TestCase
 {
+    function testGetType() {
+        $this->assertSame('enum', Reflection::getType(new \ReflectionEnum(ETest::class)));
+        $this->assertSame('attribute', Reflection::getType((new \ReflectionClass(CTest::class))->getAttributes()[0]));
+        $this->assertSame('callable', Reflection::getType(new \ReflectionCallable('strlen')));
+        $this->assertSame('class', Reflection::getType(new \ReflectionClass($this)));
+        $this->assertSame('class-constant', Reflection::getType(new \ReflectionClassConstant(CTest::class, 'NS')));
+        $this->assertSame('function', Reflection::getType(new \ReflectionFunction('strlen')));
+        $this->assertSame('method', Reflection::getType(new \ReflectionMethod(CTest::class, 'method')));
+        $this->assertSame('interface', Reflection::getType(new ReflectionInterface(ITest::class)));
+        $this->assertSame('trait', Reflection::getType(new ReflectionTrait(TTest::class)));
+        $this->assertSame('object', Reflection::getType(new ReflectionObject($this)));
+        $this->assertSame('namespace', Reflection::getType(new ReflectionNamespace('')));
+        $this->assertSame('parameter', Reflection::getType(new ReflectionParameter([CTest::class, 'method'], 'a')));
+        $this->assertSame('property', Reflection::getType(new ReflectionProperty(CTest::class, 'x')));
+        $this->assertSame('type', Reflection::getType(new ReflectionType('int')));
+    }
+
     function testGetVisibility() {
         $this->assertSame('public', Reflection::getVisibility(new \ReflectionMethod(CTest::class, 'publicMethod')));
         $this->assertSame('private', Reflection::getVisibility(new \ReflectionMethod(CTest::class, 'privateMethod')));
@@ -20,10 +37,10 @@ class ReflectionTest extends \TestCase
         $this->assertInstanceOf(ReflectionClassConstant::class, Reflection::reflectClassConstant(CTest::class, 'NS'));
         $this->assertInstanceOf(ReflectionFunction::class, Reflection::reflectFunction('strlen'));
         $this->assertInstanceOf(ReflectionInterface::class, Reflection::reflectInterface(ITest::class));
-        $this->assertInstanceOf(ReflectionMethod::class, Reflection::reflectMethod(CTest::class, 'function'));
+        $this->assertInstanceOf(ReflectionMethod::class, Reflection::reflectMethod(CTest::class, 'method'));
         $this->assertInstanceOf(ReflectionNamespace::class, Reflection::reflectNamespace(CTest::NS));
         $this->assertInstanceOf(ReflectionObject::class, Reflection::reflectObject(new CTest()));
-        $this->assertInstanceOf(ReflectionParameter::class, Reflection::reflectParameter([CTest::class, 'function'], 'a'));
+        $this->assertInstanceOf(ReflectionParameter::class, Reflection::reflectParameter([CTest::class, 'method'], 'a'));
         $this->assertInstanceOf(ReflectionProperty::class, Reflection::reflectProperty(CTest::class, 'x'));
         $this->assertInstanceOf(ReflectionTrait::class, Reflection::reflectTrait(TTest::class));
         $this->assertInstanceOf(ReflectionType::class, Reflection::reflectType('int'));
@@ -38,10 +55,15 @@ interface ITest {
     const NS = __NAMESPACE__;
 }
 
+enum ETest {
+    case A;
+}
+
+#[Attr]
 class CTest implements ITest {
     use TTest;
 
-    function function($a = null) {}
+    function method($a) {}
 
     public function publicMethod() {}
     private function privateMethod() {}
