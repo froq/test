8651ac40a1aa82baefa40dd2a1a7cc7ae7fdb583
Kerem Güneş <1508306+krmgns@users.noreply.github.com>
2024-03-08 03:58:01 +0300

    Update session tests.

--- .etc/util/session-handler.php
+++ .etc/util/session-handler.php
@@ -2,12 +2,13 @@
 namespace foo\bar;
 
 if (!class_exists(SessionHandler::class, false)) {
-    class SessionHandler extends \froq\session\AbstractHandler {
-        function open($a,$b): bool { return true; }
-        function close(): bool { return true; }
-        function write($a,$b): bool { return true; }
-        function read($a): string { return ""; }
-        function destroy($a): bool { return true; }
-        function gc($a): int { return 0; }
+    class SessionHandler extends \froq\session\SessionHandler {
+        // @cancel: Not needed anymore.
+        // function open($a,$b): bool { return true; }
+        // function close(): bool { return true; }
+        // function write($a,$b): bool { return true; }
+        // function read($a): string { return ""; }
+        // function destroy($a): bool { return true; }
+        // function gc($a): int { return 0; }
     }
 }
